473da49819d0671e443caebdeefe7d9963a249e7
Ram Pakanayev <rampakanayev@gmail.com>
2023-05-31 02:04:33 +0300

    Enhance ImageModal component styling
    1. Adjusted image sizes and positioning in the ImageGrid component to maintain consistent image display.
    2. Redefined image and overlay CSS in the ImageModal component to ensure images and overlays maintain their size and position.
    3. Updated the CSS for the modal window in ImageModal to center it on the screen and divide the window into two sections for information and image display.
    4. Ensured that the image within the modal fits within the modal window's borders.

--- client/src/components/ImageModal/ImageModal.js
+++ client/src/components/ImageModal/ImageModal.js
@@ -1,10 +1,9 @@
 // client\src\components\ImageModal.js
-
 import React, { useState } from "react";
 import Modal from "react-modal";
 import "./ImageModal.css";
 
-Modal.setAppElement("#root"); // This line is important for accessibility purposes
+Modal.setAppElement("#root");
 
 const ImageModal = ({ image }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,27 +18,29 @@ const ImageModal = ({ image }) => {
   };
 
   return (
-    <div className="image-modal" onClick={handleOpen}>
+    <div className="modal-container" onClick={handleOpen}>
       <img src={image.webformatURL} alt="" />
-      <div className="overlay">
+      <div className="modal-overlay">
         <p>Click for info</p>
       </div>
-      <Modal 
+      <Modal
         isOpen={isOpen}
         onRequestClose={handleClose}
         className="modal-content-wrapper"
       >
-        <h2>Image Details</h2>
-        <p>Views: {image.views}</p>
-        <p>Downloads: {image.downloads}</p>
-        <p>Likes: {image.likes}</p>
-        <p>Tags: {image.tags}</p>
-        <p>User: {image.user}</p>
-        <button onClick={handleClose}>Close</button>
+        <div className="modal-info">
+          <h2>Image Details</h2>
+          <p>Views: {image.views}</p>
+          <p>Downloads: {image.downloads}</p>
+          <p>Likes: {image.likes}</p>
+          <p>Tags: {image.tags}</p>
+          <p>User: {image.user}</p>
+          <button onClick={handleClose}>Close</button>
+        </div>
+        <img className="modal-image" src={image.webformatURL} alt="" />
       </Modal>
     </div>
   );
-  
 };
 
 export default ImageModal;
