f47e2291d2cac13df15aba5cfc529d5ad839cbe0
Ram Pakanayev <rampakanayev@gmail.com>
2023-05-30 21:31:43 +0300

    style and Modal changes

--- client/src/components/ImageModal/ImageModal.js
+++ client/src/components/ImageModal/ImageModal.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import Modal from "react-modal";
 import "./ImageModal.css";
 
-Modal.setAppElement("#root");
+Modal.setAppElement("#root"); // This line is important for accessibility purposes
 
 const ImageModal = ({ image }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,27 +13,27 @@ const ImageModal = ({ image }) => {
     setIsOpen(true);
   };
 
-  const handleClose = () => {
+  const handleClose = (event) => {
+    event.stopPropagation();
     setIsOpen(false);
   };
 
   return (
-    <div className="image-modal" onClick={handleOpen}>
-      <div className={`modal-content-wrapper ${isOpen ? "open" : ""}`}>
-        <img src={image.webformatURL} alt="" />
-
-        {isOpen && (
-          <div>
-            <h2>Image Details</h2>
-            <p>Views: {image.views}</p>
-            <p>Downloads: {image.downloads}</p>
-            <p>Likes: {image.likes}</p>
-            <p>Tags: {image.tags}</p>
-            <p>User: {image.user}</p>
-            <button onClick={handleClose}>Close</button>
-          </div>
-        )}
-      </div>
+    <div onClick={handleOpen}>
+      <img src={image.webformatURL} alt="" />
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={handleClose}
+        className="modal-content-wrapper"
+      >
+        <h2>Image Details</h2>
+        <p>Views: {image.views}</p>
+        <p>Downloads: {image.downloads}</p>
+        <p>Likes: {image.likes}</p>
+        <p>Tags: {image.tags}</p>
+        <p>User: {image.user}</p>
+        <button onClick={handleClose}>Close</button>
+      </Modal>
     </div>
   );
 };
